c632feed18c5a59ce37aadf6a9486ca58c2d3c54
liamcostello16 <77394754+liamcostello16@users.noreply.github.com>
2021-01-28 16:04:02 +0000

    Add files via upload

---
+++ Goldielocks/Runner.java
@@ -0,0 +1,13 @@
+package Goldielocks;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Table t = new Table();
+		
+		t.BestChairs(100, 80);
+	}
+
+}
