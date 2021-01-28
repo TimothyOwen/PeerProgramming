d7720d6efee59a3dd471d09d0b040d606b2b3903
liamcostello16 <77394754+liamcostello16@users.noreply.github.com>
2021-01-28 15:10:03 +0000

    Add files via upload

---
+++ PeerProgramming/Runner.java
@@ -0,0 +1,14 @@
+package PeerProgramming;
+
+public class Runner {
+
+	public static void main(String args[])
+	{
+		Factorials f = new Factorials();
+		
+		
+		 System.out.println(f.FactorialCalc(18));
+		 
+		 
+	}
+}
